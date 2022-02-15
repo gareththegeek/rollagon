5733b70e8085a0531797386241a4d421b8aedefc
gareththegeek <gj_williams2000@yahoo.co.uk>
2022-02-15 15:54:43 +0000

    Add mongo sanitiser

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Express } from 'express'
 import contests from './contests'
+import contestants from './contestants'
 import games from './games'
 import players from './players'
 import strife from './strife'
 
 const bind = (app: Express): void => {
+    contestants(app)
     contests(app)
     games(app)
     players(app)
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import mongoSanitize from 'express-mongo-sanitize'
 import routes from './routes'
 import controllers from './controllers'
 
@@ -11,6 +12,7 @@ const CORS_ORIGIN = process.env['CORS_ORIGIN'] ?? '*'
 app.use(cors({ origin: CORS_ORIGIN }))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(mongoSanitize())
 
 routes(app)
 
