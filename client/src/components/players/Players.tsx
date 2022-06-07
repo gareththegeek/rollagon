86de8c8b6be9f7a4a812335d54f78ec458c7cf9b
gareththegeek <gj_williams@hotmail.co.uk>
2022-06-07 15:53:25 +0100

    Make the player status indicators round

--- client/src/components/players/Players.tsx
+++ client/src/components/players/Players.tsx
@@ -10,16 +10,18 @@ export const Players = () => {
     const strifeColour = strifeConnected ? 'bg-grey-300' : 'bg-transparent'
 
     return (
-        <ul>
-            <li>
-                <div className={`${strifeColour} h-4 w-4 border-2 inline-block`} /> Strife Player
+        <ul className="flex flex-col items-end">
+            <li className="flex items-center">
+                Strife Player
+                <div className={`${strifeColour} h-4 w-4 border-2 rounded-lg ml-2 inline-block`} />
             </li>
             {players.map((player) => {
                 const isConnected = connections.includes(player.id ?? '')
                 const colour = isConnected ? 'bg-grey-300' : 'bg-transparent'
                 return (
-                    <li key={player.id}>
-                        <div className={`${colour} h-4 w-4 border-2 inline-block`} /> {player.name}
+                    <li key={player.id} className="flex items-center">
+                        {player.name}
+                        <div className={`${colour} h-4 w-4 border-2 rounded-lg ml-2 inline-block`} />
                     </li>
                 )
             })}
