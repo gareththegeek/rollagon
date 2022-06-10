80ac0e7107f659ddd7a8db7ed7d0ed216532187c
gareththegeek <gj_williams@hotmail.co.uk>
2022-06-10 11:49:00 +0100

    Address accessibility issues on Splash

--- client/src/screens/splash/Splash.tsx
+++ client/src/screens/splash/Splash.tsx
@@ -22,28 +22,50 @@ export const Splash = () => {
     const [text, setText] = useState('')
 
     return (
-        <div className="container max-w-screen-md mx-auto flex flex-col pt-16">
-            <H1>Agon Dice Roller</H1>
-            <p className="text-base font-normal pb-6">This is a fan-made dice-rolling app for AGON. AGON is an action-packed roleplaying game about epic Heroes who face trials from the Gods in an ancient world of myth and legend. Learn more about it, and the Paragon system, at <A href="http://agon-rpg.com">agon-rpg.com</A></p>
-            <H3>Start a New Session</H3>
-            <p className="text-base pb-6">Start a new session. You'll be able to invite other players with a unique URL.</p>
-            <BigButton className="mb-6" onClick={createGameClick(dispatch, navigate)}>
-                Start New Game
-            </BigButton>
-            <H3>Have an Invite Link?</H3>
-            <p className="text-base pb-6">Paste it below or in your browser and click Join Game.</p>
-            <Input
-                className="mb-6"
-                value={text}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => setText(e.target.value)}
-                placeholder="Existing game id"
-            />
-            <BigButton className="mb-6" onClick={() => navigate(`/join/${text}`)}>Join Game</BigButton>
-            <H3>About this App</H3>
-            <p className="text-base pb-3">At least one person playing should be familiar with the AGON rulebook.</p>
-            <p className="text-base pb-3">This app is a lightweight dicerolling app, focused on resolving and narrating AGON Contests. It is not a campaign or character tracker - you'll still need to manage that on your own.</p>
-            <p className="text-base pb-3">This app was created by <A href="">@gareth</A> and <A href="">@sporgory</A> with the help of the <A href="">AGON fan Discord community</A>.</p>
-            <p className="text-base pb-3 mb-64">If you’d like to give feedback or get involved, check out the <A href="https://github.com/gareththegeek/rollagon">GitHub repo</A>.</p>
+        <div className="container max-w-screen-md mx-auto pt-16">
+            <main className="flex flex-col">
+                <H1>Agon Dice Roller</H1>
+                <p className="text-base font-normal pb-6">
+                    This is a fan-made dice-rolling app for AGON. AGON is an action-packed roleplaying game about epic
+                    Heroes who face trials from the Gods in an ancient world of myth and legend. Learn more about it,
+                    and the Paragon system, at <A href="http://agon-rpg.com">agon-rpg.com</A>
+                </p>
+                <H3>Start a New Session</H3>
+                <p className="text-base pb-6">
+                    Start a new session. You'll be able to invite other players with a unique URL.
+                </p>
+                <BigButton className="mb-6" onClick={createGameClick(dispatch, navigate)}>
+                    Start New Game
+                </BigButton>
+                <H3>Have an Invite Link?</H3>
+                <p className="text-base pb-6">Paste it below or in your browser and click Join Game.</p>
+                <Input
+                    className="mb-6"
+                    aria-label="Existing game id"
+                    value={text}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setText(e.target.value)}
+                    placeholder="Existing game id"
+                />
+                <BigButton className="mb-6" onClick={() => navigate(`/join/${text}`)}>
+                    Join Game
+                </BigButton>
+            </main>
+            <aside role="complementary" className="flex flex-col">
+                <H3>About this App</H3>
+                <p className="text-base pb-3">At least one person playing should be familiar with the AGON rulebook.</p>
+                <p className="text-base pb-3">
+                    This app is a lightweight dicerolling app, focused on resolving and narrating AGON Contests. It is
+                    not a campaign or character tracker - you'll still need to manage that on your own.
+                </p>
+                <p className="text-base pb-3">
+                    This app was created by <A href="">@gareth</A> and <A href="">@sporgory</A> with the help of the{' '}
+                    <A href="">AGON fan Discord community</A>.
+                </p>
+                <p className="text-base pb-3 mb-64">
+                    If you’d like to give feedback or get involved, check out the{' '}
+                    <A href="https://github.com/gareththegeek/rollagon">GitHub repo</A>.
+                </p>
+            </aside>
         </div>
     )
 }
